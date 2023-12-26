1306c249d89466d3070ec6b144be270b0eaec180
wardonne <Wardonne@yandex.ru>
2023-12-27 03:30:18 +0400

    gen

--- last/alg.h
+++ last/alg.h
@@ -14,6 +14,7 @@
 using namespace std;
 
 namespace algorithm {
+
     int generate_random_number(int min, int max, int seed) {
         mt19937 mt(seed);
         uniform_int_distribution<int> distribution(min, max);
@@ -38,6 +39,26 @@ namespace algorithm {
                     mass[j + 1] = temp;
                 }
             }
+        }
+        return cur_stat;
+    }
+    template<typename T>
+    stats comb_sort(vector<T>& mass) {
+        stats cur_stat;
+        double factor = 1.2473309;
+        int step = mass.size() - 1;
+        while (step >= 1)
+        {
+            for (int i = 0; i + step < mass.size(); i++)
+            {
+                cur_stat.comparison_count++;
+                if (mass[i] > mass[i + step])
+                {
+                    cur_stat.copy_count++;
+                    std::swap(mass[i], mass[i + step]);
+                }
+            }
+            step /= factor;
         }
         return cur_stat;
     }
