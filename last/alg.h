dd2a6982655d9d753cac6fe3c1ef2feccf977c11
wardonne <Wardonne@yandex.ru>
2023-12-27 06:03:37 +0400

    sorting

--- last/alg.h
+++ last/alg.h
@@ -16,12 +16,12 @@
 using namespace std;
 
 namespace algorithm {
-
     int generate_random_number(int min, int max, int seed) {
         mt19937 mt(seed);
         uniform_int_distribution<int> distribution(min, max);
         return distribution(mt);
     }
+
     struct stats {
         size_t comparison_count = 0;
         size_t copy_count = 0;
@@ -38,16 +38,13 @@ namespace algorithm {
         }
         return stream;
     }
+
     template<typename T>
     ostream& operator<<(ostream& stream, vector<T> val) {
         for (int i = 0; i < val.size(); i++) {
             stream << setw(3) << val[i] << " ";
         }
         return stream;
-    }
-    template<typename T>
-    stats sort(vector<T>& mass, stats func(vector<T>&)) {
-        return func(mass);
     }
     stats average(vector<stats> mass) {
         stats stat;
@@ -58,6 +55,57 @@ namespace algorithm {
         stat.comparison_count /= mass.size();
         stat.copy_count /= mass.size();
         return stat;
+    }
+    template<typename T>
+    void fill_file(stats func(vector<T>&), string path, int count, int sort_type, int seed) {
+        fstream file;
+        file.open(path, std::ios::app);
+        vector<int> vals = { 1,2,3,4,5,6,7,8,9,10,25,50,100 };
+        if (file) {
+            for (int i = 0; i < vals.size(); i++) {
+                /*cout << vals[i] * 1000 << endl;*/
+                vector<stats> res = test_sort(func, vals[i] * 1000, count, sort_type, seed);
+                file << vals[i] * 1000 << " ";
+                file << average(res);
+            }
+        }
+        else {
+            cout << "ERROR";
+        }
+        file.close();
+    }
+    
+    template<typename T>
+    vector<stats> test_sort(stats func(vector<T>&), int limit, int count, int mass_type, int seed) {
+        //0 - sort; 1 - unsorted; 2 - random
+        vector<stats> results;
+        int seed_helper = 0;
+        for (int i = 0; i <= count - 1; i++) {
+            vector<int> mass;
+            switch (mass_type)
+            {
+            case 0:
+                for (int j = 0; j < limit; j++) {
+                    mass.push_back(j);
+                }
+                break;
+            case 1:
+                for (int j = 0; j < limit; j++) {
+                    mass.push_back(limit - j);
+                }
+                break;
+            case 2:
+                for (int j = 0; j < limit; j++) {
+                    mass.push_back(generate_random_number(1, 100, seed+seed_helper));
+                    seed_helper++;
+                }
+                break;
+            }
+
+            cout << mass<< endl;
+            results.push_back(func(mass));
+        }
+        return results;
     }
     template<typename T>
     stats bubble_sort(vector<T>& mass) {
@@ -80,7 +128,7 @@ namespace algorithm {
     stats comb_sort(vector<T>& mass) {
         stats cur_stat;
         double factor = 1.2473309;
-        int step = mass.size() - 1;
+        size_t step = mass.size() - 1;
         while (step >= 1)
         {
             for (int i = 0; i + step < mass.size(); i++)
--- last/main.cpp
+++ last/main.cpp
@@ -9,6 +9,35 @@
 #include "alg.h"
 using namespace algorithm;
 int main(int argc, const char * argv[]) {
-    int seed = 777;
+    int seed = 983747;
+    const int limit = 100;  // adjust the size of the array as needed
+    const int count = 1;   // adjust the number of tests as needed
+
+    // Test with sorted input
+    vector<algorithm::stats> bubble_sort_sorted_stats = algorithm::test_sort<int>(algorithm::bubble_sort, limit, count, 0, seed);
+    algorithm::stats bubble_sort_sorted_avg_stats = algorithm::average(bubble_sort_sorted_stats);
+    cout << "Bubble Sort (Sorted Input) Avg Stats: " << bubble_sort_sorted_avg_stats << endl;
+
+    // Test with reverse-sorted input
+    vector<algorithm::stats> comb_sort_reverse_sorted_stats = algorithm::test_sort<int>(algorithm::comb_sort, limit, count, 1, seed);
+    algorithm::stats comb_sort_reverse_sorted_avg_stats = algorithm::average(comb_sort_reverse_sorted_stats);
+    cout << "Comb Sort (Reverse Sorted Input) Avg Stats: " << comb_sort_reverse_sorted_avg_stats << endl;
+
+    // Test with random input
+    vector<algorithm::stats> shaker_sort_random_stats = algorithm::test_sort<int>(algorithm::shaker_sort, limit, count, 2, seed);
+    algorithm::stats shaker_sort_random_avg_stats = algorithm::average(shaker_sort_random_stats);
+    cout << "Shaker Sort (Random Input) Avg Stats: " << shaker_sort_random_avg_stats << endl;
+
+
+//    fill_file(shaker_sort<int>, "/Users/wardonne/Documents/last/last/rand_data_shaker_sort.txt", 100, 2, seed);
+//    fill_file(comb_sort<int>, "/Users/wardonne/Documents/last/last/rand_data_comb_sort.txt", 100, 2, seed);
+//    fill_file(bubble_sort<int>, "/Users/wardonne/Documents/last/last/rand_data_bubble_sort.txt", 100, 2, seed);
+//    fill_file(bubble_sort<int>, "/Users/wardonne/Documents/last/last/unsorted_bubble.txt", 1, 1, seed);
+//    fill_file(shaker_sort<int>, "/Users/wardonne/Documents/last/last/unsorted_shaker.txt", 1, 1, seed);
+//    fill_file(comb_sort<int>, "/Users/wardonne/Documents/last/last/unsorted_comb.txt", 1, 1, seed);
+//    fill_file(bubble_sort<int>, "/Users/wardonne/Documents/last/last/sorted_bubble.txt", 1, 0, seed);
+//    fill_file(shaker_sort<int>, "/Users/wardonne/Documents/last/last/sorted_shaker.txt", 1, 0, seed);
+//    fill_file(comb_sort<int>, "/Users/wardonne/Documents/last/last/sorted_comb.txt", 1, 0, seed);
+
     return 0;
 }
